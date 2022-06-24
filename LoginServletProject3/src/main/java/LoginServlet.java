e0ad5e3c8a98d7f695d911febe4553991efe8062
Thae Su Paing <106294159+ThaeSuPaing699@users.noreply.github.com>
2022-06-24 13:51:35 +0630

    Add files via upload

---
+++ LoginServletProject3/src/main/java/LoginServlet.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String username= request.getParameter("username");
+		String pw = request.getParameter("password");
+		String login = request.getParameter("loginButton");
+		
+		if(username.equals("ThaeSuPaing")&& pw.equals("1234")) {
+			out.println("<center><h2 style='color: purple;'>Welcome "+username+"</h2></center>");
+			
+		}else {
+			out.println("<h2 style='color: red;'>login fail!</h2>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
